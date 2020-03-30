59afd02517397d514de6e950e147b27f3aafc85e
Ali Y <43638010+HirakoShiniji@users.noreply.github.com>
2020-03-30 20:20:36 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowtopiaSpoofer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GrowtopiaSpoofer by Hirako#4480"); Console.WriteLine("HTTP Server is running!");
+         
+            HTTP.gtfakeserver = new HttpListener();
+            HTTP.gtfakeserver.Prefixes.Add(HTTP.url);
+
+            HTTP.Spoofer();
+
+        }
+    }
+}
