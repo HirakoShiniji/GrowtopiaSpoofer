3ca915c8529f9b04347eaed9e192f10d62028916
Ali Y <43638010+HirakoShiniji@users.noreply.github.com>
2020-04-05 11:36:41 +0300

    Create list.cs

---
+++ list.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrowtopiaSpoofer
+{
+    class list
+    {
+       public static long func_mac_address = 0x476894;
+       public static long platformID_address = 0x476873;
+       public static long reconnect = 0x4768A2;
+        public static long game_version= 0x45EDFC;
+        public static long located = 0x479390;
+    }
+}
